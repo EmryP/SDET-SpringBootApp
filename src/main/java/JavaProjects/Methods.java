bebd214437418f5b08dfbb2939a494c931a141d9
EmrayP <emraypala@gmail.com>
2021-03-07 16:25:24 -0600

    Methods

---
+++ src/main/java/JavaProjects/Methods.java
@@ -0,0 +1,20 @@
+package src.main.java.JavaProjects;
+
+public class  Methods {
+   public void methodA(){
+       System.out.println("methodA() is called");
+   }
+   public void methodA(String str){
+       System.out.println("methodA(String str) is called");
+   }
+   public String methodB(){
+       return "method B() is called";
+   }
+   public String methodB(int x){
+       return "methodB(int x) is called";
+   }
+   public static void methodC(){
+       System.out.println("methodC() is called");
+   }
+
+}
